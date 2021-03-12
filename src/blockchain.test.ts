7dfce55001474a2ca7d3b7b372c278c930c7229f
jolee <jolee@venacorp.com>
2021-03-12 17:56:40 -0500

    2021/03/11

--- src/blockchain.test.ts
+++ src/blockchain.test.ts
@@ -2,8 +2,16 @@ import Blockchain from './blockchain';
 import Block from './block';
 
 describe('Blockchain', () => {
-    const blockchain = new Blockchain();
+    let blockchain: Blockchain, shorterChain: Blockchain;
 
+    beforeEach(() => {
+        blockchain = new Blockchain();
+        blockchain.addBlock('Bears');
+        blockchain.addBlock('Beets');
+        blockchain.addBlock('Battlestar Galactica');
+
+        shorterChain = new Blockchain();
+    });
     it('contains a `chain` Array instance', () => {
         expect(blockchain.chain instanceof Array).toBe(true);
     });
@@ -14,7 +22,67 @@ describe('Blockchain', () => {
 
     it('adds a new block to the chain', () => {
         const newData = 'foo bar';
-        blockchain.addBlock({ data: newData });
+        blockchain.addBlock(newData);
         expect(blockchain.chain[blockchain.chain.length-1]).toHaveProperty('data', newData);
+    });
+
+    describe('isValidChain()', () => {
+        describe('when the chain does not start with the genesis block', () => {
+            it('returns false', () => {
+                blockchain.chain[0].data = 'fake-genesis';
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+            });
+        });
+
+        describe('when the chain starts with the genesis block and has multiple blocks', () => {
+            describe('and a lastHash reference has changed', () => {
+                it('returns false', () => {
+                    blockchain.chain[2].lastHash = 'broken-lastHash';
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                });
+            });
+            describe('and the chain contains a block with an invalid field', () => {
+                it('returns false', () => {
+                    blockchain.chain[2].data = 'some-bad-and-evil-data';
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+
+                });
+            });
+            describe('and the chain does not contain any invalid blocks', () => {
+                it('returns true', () => {
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+                });
+            });
+        });
+        
+    });
+
+    describe('replaceChain()', () => {
+        describe('when the new chain is not longer', () => {
+            it('does not replace the chain', () => {
+                const ogChain = blockchain.chain;
+                // @ts-ignore
+                shorterChain.chain[0] = { new: 'chain' };
+                blockchain.replaceChain(shorterChain.chain);
+                expect(blockchain.chain).toEqual(ogChain);
+            });
+        });
+
+        describe('when the new chain is longer', () => {
+            describe('and the chain is invalid', () => {
+                it('does not replace the chain', () => {
+                    const ogChain = shorterChain.chain;
+                    blockchain.chain[2].hash = 'wrong-hash';
+                    shorterChain.replaceChain(blockchain.chain);
+                    expect(shorterChain.chain).toEqual(ogChain);
+                });
+            });
+            describe('and the chain is valid', () => {
+                it('replaces the chain', () => {
+                    shorterChain.replaceChain(blockchain.chain);
+                    expect(shorterChain.chain).toEqual(blockchain.chain);
+                });
+            });
+        });
     });
 });
--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,4 +1,5 @@
 import Block from "./block";
+import { cryptoHash } from "./crypto-hash";
 
 class Blockchain {
     chain: Block[];
@@ -13,6 +14,16 @@ class Blockchain {
             data
         });
         this.chain.push(newBlock);
+    }
+
+    static isValidChain(chain: Block[]): boolean {
+        if (JSON.stringify(Block.genesis()) !== JSON.stringify(chain[0])) return false;
+        return !chain.slice(1).some(({ timestamp, lastHash, hash, data }) => cryptoHash(String(timestamp), lastHash, data) !== hash)
+    }
+
+    replaceChain(chain: Block[]) {
+        if (this.chain.length > chain.length || !Blockchain.isValidChain(chain)) return;
+        this.chain = chain;
     }
 }
 
